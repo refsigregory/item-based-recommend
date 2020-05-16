a4319d5d27ef5a063b4fe7054149b2c47c0aee9c
Refsi Gregory <refsisangkay@gmail.com>
2020-05-16 20:13:23 +0800

    first

---
+++ sample_list.php
@@ -0,0 +1,13 @@
+<?php
+
+$books =  array(
+                
+    "refsi" => array("angel" => 5, "yudi" => 5, "bob" => 1, "vicky"=>5, "billy" => 5),
+    "angel" => array("bob" => 5, "yudi" => 5, "billy" => 5, "refsi"=>4, "indah" => 4, "brenda" => 5),
+    "yudi" => array("angel"=>5, "billy"=>5,"bob"=>3, "refsi"=>5, "olivia" => 5, "felix" =>3, "sela" =>4),
+    "vicky" => array("refsi" => 5, "andre" => 5),
+    "jimmy" => array("karen"=>4),
+    "elzha" => array()
+);
+
+?>
